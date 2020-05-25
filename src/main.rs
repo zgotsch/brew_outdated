bbe49dcc35f63f71de680e055584e8c2510806b4
Zach Gotsch <orbipedis@gmail.com>
2020-05-25 00:12:02 -0700

    Use FuturesUnordered and make output pretty

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-#![feature(type_alias_impl_trait)]
-#![feature(impl_trait_in_bindings)]
-
 #[macro_use]
 extern crate lazy_static;
 
-use futures::future::join_all;
 use futures::join;
+use futures::stream::futures_unordered::FuturesUnordered;
+use futures::stream::StreamExt;
 use regex::Regex;
+use std::collections::HashMap;
 use std::collections::HashSet;
+use std::ffi::OsString;
 use std::path::Path;
+use std::rc::Rc;
+use yansi::Paint;
 
 mod history;
 mod homebrew;
@@ -37,16 +39,43 @@ async fn main() -> Result<(), String> {
     // run brew outdated
     // get things that are in both and print them nicely
 
-    let outdated_executables = async {
-        let outdated = homebrew::outdated().await.unwrap();
-        let executables = join_all(outdated.iter().map(|p| {
-            homebrew::executables(&p.package_name, &p.installed_versions.last().unwrap())
-        }))
-        .await
-        .into_iter()
-        .flatten()
-        .collect::<HashSet<std::ffi::OsString>>();
-        return executables;
+    // a guess for a reasonable upper bound of executables per brew package
+    const EXECUTABLES_PER_PACKAGE: usize = 10;
+    let brew_fut = async {
+        let outdated: Vec<homebrew::BrewOutdatedEntry> = homebrew::outdated().await.unwrap();
+        let outdated_executable_to_package: HashMap<OsString, Rc<homebrew::BrewOutdatedEntry>> =
+            HashMap::with_capacity(outdated.len() * EXECUTABLES_PER_PACKAGE);
+        let outdated_executables: HashSet<OsString> =
+            HashSet::with_capacity(outdated.len() * EXECUTABLES_PER_PACKAGE);
+
+        let executables_futures = FuturesUnordered::new();
+        for outdated_entry in outdated.into_iter() {
+            executables_futures.push(async {
+                let executables = homebrew::executables(
+                    &outdated_entry.package_name,
+                    &outdated_entry.installed_versions.last().unwrap(),
+                )
+                .await;
+                (outdated_entry, executables)
+            });
+        }
+
+        executables_futures
+            .fold(
+                (outdated_executable_to_package, outdated_executables),
+                |(mut outdated_executable_to_package, mut outdated_executables),
+                 (entry, executables)| async move {
+                    let entry = Rc::new(entry);
+                    for executable in executables {
+                        outdated_executables.insert(executable.clone());
+                        outdated_executable_to_package.insert(executable, entry.clone());
+                    }
+                    (outdated_executable_to_package, outdated_executables)
+                },
+            )
+            .await
+
+        // executables_futures.for_each_concurrent(None, || async {});
     };
 
     let used_executables = async {
@@ -58,12 +87,36 @@ async fn main() -> Result<(), String> {
             .collect::<HashSet<std::ffi::OsString>>()
     };
 
-    let (outdated_executables, used_executables) = join!(outdated_executables, used_executables);
+    let ((outdated_executable_to_package, outdated_executables), used_executables) =
+        join!(brew_fut, used_executables);
 
-    println!(
-        "{:#?}",
-        outdated_executables.intersection(&used_executables)
-    );
+    let mut should_update_executables = outdated_executables
+        .intersection(&used_executables)
+        .peekable();
+
+    if should_update_executables.peek().is_some() {
+        let mut should_update_packages = Vec::with_capacity(outdated_executables.len());
+
+        println!("You have recently used out-of-date executables which are managed by `brew`.");
+        println!("Consider updating the following:");
+        for executable in should_update_executables {
+            let entry = outdated_executable_to_package.get(executable).unwrap();
+            println!(
+                "\t{} (installed: {}, available: {})",
+                Paint::new(executable.to_string_lossy()).bold(),
+                Paint::red(&entry.installed_versions.last().unwrap()).bold(),
+                Paint::green(&entry.current_version).bold()
+            );
+            should_update_packages.push(entry.package_name.as_str());
+        }
+        let brew_cmd = vec![vec!["brew", "upgrade"], should_update_packages]
+            .concat()
+            .join(" ");
+        println!(
+            "To upgrade all of these in one command, run `{}`",
+            Paint::new(brew_cmd).bold()
+        );
+    }
 
     Ok(())
 }
